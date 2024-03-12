82844ba38c2c786ec0696e73d0c3679fb2005972
Sebastian Estay <sbestay84@gmail.com>
2024-03-12 20:58:13 +0100

    shadow variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,6 @@
 
-const TWO_HOURS_IN_SECONDS: u32 = 2*60*60;
-
 fn main() {
-    let mut x = 5; 
-    println!("the value of x is: {x}");
-    if x < TWO_HOURS_IN_SECONDS {
-      x = 6;
-      println!("the value of x is: {x}");
-    }
+  let x = 5; 
+  let x = x + 1;
+  println!("the value of x is: {x}");
 }
