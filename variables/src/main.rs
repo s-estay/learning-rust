dffef5e9b21135636ef7c5db14ee662540ef485f
Sebastian Estay <sbestay84@gmail.com>
2024-03-12 20:52:27 +0100

    declare and use const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,11 @@
 
+const TWO_HOURS_IN_SECONDS: u32 = 2*60*60;
+
 fn main() {
     let mut x = 5; 
     println!("the value of x is: {x}");
-    x = 6;
-    println!("the value of x is: {x}");
+    if x < TWO_HOURS_IN_SECONDS {
+      x = 6;
+      println!("the value of x is: {x}");
+    }
 }
