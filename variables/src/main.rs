5f50a73892509eacd59d278019dcafabff51dac0
Sebastian Estay <sbestay84@gmail.com>
2024-03-12 21:08:42 +0100

    change type while shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,7 @@
 
 fn main() {
-  let x = 5; 
-  let x = x + 1;
-  {
-    let x = x * 2;
-    println!("the value of x is: {x}");
-  }
-  println!("the value of x is: {x}");
+  let abc = "abc";
+  println!("text: {abc}");
+  let abc = abc.len();
+  println!("text: {abc}");
 }
