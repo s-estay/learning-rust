5f95e6a68c14de1b19969004741c341aa57aa71e
Sebastian Estay <sbestay84@gmail.com>
2024-03-12 20:34:39 +0100

    declare mutable variable and update its value

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,16 +1,7 @@
 
-//const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
 fn main() {
-    let x = 5; // declare new variable
+    let mut x = 5; 
     println!("the value of x is: {x}");
-    {
-        let x = x * 2; // shadow variable x inside inner scope
-        println!("the value of x is: {x}");
-    }
-    let x = x + 1; // shadow variable x
+    x = 6;
     println!("the value of x is: {x}");
-
-    let spaces = "   "; // string type
-    let spaces = spaces.len(); // number type
 }
